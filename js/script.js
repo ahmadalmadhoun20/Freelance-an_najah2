04713401a15289bb68d0b7802cc790b42929ebe4
Ahmad <almadhouna849@gmail.com>
2023-05-29 20:48:44 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var btn = document.querySelector('header .menu .bars');
+var menu = document.querySelector('header .menu ul');
+
+btn.onclick=() => {
+    menu.classList.toggle('show');
+}
+
+var btn = document.querySelector('#btn1');
+var btns = document.querySelector('#btn2');
+var modls = document.querySelector('.modal');
+btn.onclick = ()=>{
+    modls.classList.add('ahmad');
+}
+btns.onclick = ()=>{
+    modls.classList.remove('ahmad');
+}
+btn_header.onclick = () => {
+    head.style.display = 'block';
+}
+
